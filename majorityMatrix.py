0a2e31e8978ed2314279fcd2cb15bafba8847a86
Michael Sodamin <>
2022-07-09 19:28:32 +0200

    Lets go

---
+++ majorityMatrix.py
@@ -0,0 +1,8 @@
+""" Class for majority matrix """
+class MajorityMatrix:
+    """ Class for majority matrix """
+    def __init__(self, matrix):
+        """ Initialize matrix """
+        self.matrix = matrix
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
